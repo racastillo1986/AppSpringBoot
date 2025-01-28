f5e4ebc53b88659425ef01d2a4d2005f311c53a8
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-01-28 14:14:52 -0500

    nuevos operadores para streams

--- JavaStreams/JavaStreams/src/main/java/org/example/streams/StreamExample.java
+++ JavaStreams/JavaStreams/src/main/java/org/example/streams/StreamExample.java
@@ -55,7 +55,7 @@ public class StreamExample {
         // collect(): Recoge todos los elementos en una coleccion
         System.out.println("********************* collect() *************************");
         List<String> result1 = names.stream()
-                .map( (name) -> name.toUpperCase() )
+                .map((name) -> name.toUpperCase())
                 .collect(Collectors.toList()); //.toList(); a partir de java17
 
         result1.stream().forEach(System.out::println);
@@ -65,5 +65,38 @@ public class StreamExample {
         names.stream()
                 .distinct()
                 .forEach(System.out::println);
+
+        // limit(): Limita el numero de elementos procesados
+        System.out.println("********************* limit() *************************");
+        names.stream()
+                .limit(3)
+                .forEach(System.out::println);
+
+        // skip(): Omite numero especifico de elementos
+        System.out.println("********************* skip() *************************");
+        names.stream()
+                .skip(3)
+                .forEach(System.out::println);
+
+        //anyMatch(): Verifica si algun elemento cumple una condicion
+        // Resumido: .anyMatch((name) -> name.startsWith("J"))
+        System.out.println("********************* anyMatch() *************************");
+        boolean result2 = names.stream()
+                .anyMatch((name) -> {
+                    return name.startsWith("J");
+                });
+        System.out.println(result2);
+
+        //allMatch(): Verifica si TODOS los elementos cumplen la condicion
+        System.out.println("********************* allMatch() *************************");
+        boolean result3 = names.stream()
+                .allMatch((name) -> name.startsWith("A"));
+        System.out.println(result3);
+
+        //noneMatch(): Verifica si NINGUN elemento cumple la condicion
+        System.out.println("********************* anoneMatch() *************************");
+        boolean result4 = names.stream()
+                .noneMatch((name) -> name.length() == 10);
+        System.out.println(result4);
     }
 }
