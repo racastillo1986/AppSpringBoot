1b3becedbbfd9573343900c403aa03b598e601d9
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-02-12 12:32:53 -0500

    ApiGatewayEurekaMicroservices OK

--- MicroServices/SpringMicroServices/microservice-course/src/main/java/com/microservice/course/persistence/ICoursesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservice.course.persistence;
-
-import com.microservice.course.entities.Course;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICoursesRepository extends CrudRepository<Course, Long> {
-}
--- MicroServices/SpringMicroServices/microservice-course/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO courses (name, teacher) VALUES ('Matematicas', 'Profesor A');
-INSERT INTO courses (name, teacher) VALUES ('Física', 'Profesor B');
-INSERT INTO courses (name, teacher) VALUES ('Historia', 'Profesor C');
-INSERT INTO courses (name, teacher) VALUES ('Literatura', 'Profesor D');
-INSERT INTO courses (name, teacher) VALUES ('Biología', 'Profesor E');
