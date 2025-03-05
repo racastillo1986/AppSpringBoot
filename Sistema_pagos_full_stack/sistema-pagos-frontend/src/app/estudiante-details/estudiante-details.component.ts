1c2342b2d9b2c13a3dcdf0cbb163440100544c86
racastillo1986 <racastillo@coop15abril.fin.ec>
2025-03-05 16:36:49 -0500

    estudiante-details OK

--- Sistema_pagos_full_stack/sistema-pagos-frontend/src/app/estudiante-details/estudiante-details.component.ts
+++ Sistema_pagos_full_stack/sistema-pagos-frontend/src/app/estudiante-details/estudiante-details.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pago } from '../models/estudiantes.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EstudiantesService } from '../services/estudiantes.service';
 
 @Component({
   selector: 'app-estudiante-details',
   templateUrl: './estudiante-details.component.html',
   styleUrl: './estudiante-details.component.css'
 })
-export class EstudianteDetailsComponent {
+export class EstudianteDetailsComponent implements OnInit{
+
+  estudianteCodigo!: string;
+  pagosEstudiane!: Array<Pago>;
+  pagosDataSource!: MatTableDataSource<Pago>;
+
+  public displayedColumns = ['id', 'fecha', 'cantidad', 'type', 'status', 'nombre'];
+
+  constructor(private activatedRoute: ActivatedRoute, private estudiantesService: EstudiantesService, private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+    this.estudianteCodigo = this.activatedRoute.snapshot.params['codigo'];
+    this.estudiantesService.getPagosDeEstudiante(this.estudianteCodigo).subscribe({
+      next: value => {
+        this.pagosEstudiane = value;
+        this.pagosDataSource = new MatTableDataSource<Pago>(this.pagosEstudiane);
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
+
+  agregarPago() {
+    this.router.navigateByUrl(`/admin/new-pago/${this.estudianteCodigo}`);
+  }
 
 }
